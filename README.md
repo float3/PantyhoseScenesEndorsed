1862958e83d1d320b3ec8ed3306e90c62ae123d6
Float3 <86748455+Float3@users.noreply.github.com>
2022-09-29 00:11:54 +0200

    skip if directory exists

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # What is this?
-This is supposed to produce [tachiyomi](https://tachiyomi.org) compatible manga https://tachiyomi.org/help/guides/local-manga/#folder-structure 
+This is supposed to produce [tachiyomi](https://tachiyomi.org) compatible manga https://tachiyomi.org/help/guides/local-manga/#folder-structure
 
 I haven't tested yet
-
 # Requirements
 ```cmd
 pip install beautifulsoup4
 ```
-
 # Running
-The following command will download 300 chapters of vagabond
+The following commands will download 300 chapters of vagabond in different directories
 ```cmd
 python scraper.py https://vagabond-chapters.com/manga/vagabond-chapter- 300
+python scraper.py https://vagabond-chapters.com/manga/vagabond-chapter- 300 C:\Manga\Vagabond\
+python scraper.py https://vagabond-chapters.com/manga/vagabond-chapter- 300 Manga\Vagabond\
 ```
--- scraper.py
+++ scraper.py
@@ -5,29 +5,35 @@ import os
 import sys
 
 
-def iterate_images(result, i, chapter):
+def iterate_images(result, i, path):
     if result.status_code == 200:
-        with open('ch' + str(chapter) + '\\' + str(i) + ".png", 'wb') as f:
+        with open(path + '\\' + str(i) + ".png", "wb") as f:
             shutil.copyfileobj(result.raw, f)
-        print('Image sucessfully Downloaded: ', str(i) + ".png")
+        print("Image successfully Downloaded: ", str(i) + ".png")
     else:
-        print('Image Couldn\'t be retrieved: ', str(i) + ".png")
+        print("Image Couldn't be retrieved: ", str(i) + ".png")
 
 
 def main():
     for chapter in range(1, chapters + 1):
         print("starting chapter: " + str(chapter))
-        os.mkdir('ch' + str(chapter))
+        path = output + "ch" + str(chapter)
+        if os.path.isdir(path):
+            continue
+        os.mkdir(path)
         html = requests.get(source + str(chapter))
         soup = BeautifulSoup(html.content, features="html.parser")
         index = 0
 
-        for imgtag in soup.find_all('img'):
-            res = requests.get(imgtag['src'], stream=True)
-            iterate_images(res, index, chapter)
+        for imgtag in soup.find_all("img"):
+            res = requests.get(imgtag["src"], stream=True)
+            iterate_images(res, index, path)
             index += 1
 
 
-source = str(sys.argv[1])
+source = sys.argv[1]
 chapters = int(sys.argv[2])
+output = ""
+if len(sys.argv) == 4:
+    output = sys.argv[3]
 main()
