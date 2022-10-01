c36b68ab7dce3179631a090456862056c9b1f11d
3 <86748455+Float3@users.noreply.github.com>
2022-10-01 15:35:16 +0200

    copilot magic

--- scraper.py
+++ scraper.py
@@ -1,40 +1,43 @@
-import os
-import shutil
-import sys
+import argparse
+import asyncio
+from pathlib import Path
 
-import requests
+import aiohttp
 from bs4 import BeautifulSoup
 
 
-def iterate_images(result, i, path):
-    if result.status_code == 200:
-        with open(path + '\\' + str(i) + ".png", "wb") as f:
-            shutil.copyfileobj(result.raw, f)
-        print("Image successfully Downloaded: ", str(i) + ".png")
-    else:
-        print("Image Couldn't be retrieved: ", str(i) + ".png")
+# Download all images from a given URL and save them to a folder asynchonously
+async def download_images(url, folder):
+    if not folder.exists():
+        folder.mkdir(parents=True)
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            html = await response.text()
+            soup = BeautifulSoup(html, 'html.parser')
+            images = soup.find_all('img')
+            for index, image in enumerate(images):
+                src = image['src']
+                filename = f'{index}.jpg'
+                async with session.get(src) as response:
+                    with open(f'{folder}/{filename}', 'wb') as f:
+                        while True:
+                            chunk = await response.content.read(1024)
+                            if not chunk:
+                                break
+                            f.write(chunk)
+                print(f'Downloaded {filename}')
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('url', help='URL to download images from')
+    parser.add_argument('max_chapters', help='Maximum number of chapters to download')
+    parser.add_argument('folder', help='Folder to save images to', nargs='?')
+    args = parser.parse_args()
 
-def main():
-    for chapter in range(1, chapters + 1):
-        print("starting chapter: " + str(chapter))
-        path = output + "ch" + str(chapter)
-        if os.path.isdir(path):
-            continue
-        os.mkdir(path)
-        html = requests.get(source + str(chapter))
-        soup = BeautifulSoup(html.content, features="html.parser")
-        index = 0
+    outpath = Path(args.folder) if args.folder else Path(__file__).parent / "down"
 
-        for imgtag in soup.find_all("img"):
-            res = requests.get(imgtag["src"], stream=True)
-            iterate_images(res, index, path)
-            index += 1
-
-
-source = sys.argv[1]
-chapters = int(sys.argv[2])
-output = ""
-if len(sys.argv) == 4:
-    output = sys.argv[3]
-main()
+    loop = asyncio.get_event_loop()
+    tasks = []
+    for i in range(1, int(args.max_chapters) + 1):
+        tasks.append(download_images(args.url.format(chapter=i), outpath / f"ch{i}"))
+    loop.run_until_complete(asyncio.wait(tasks))
